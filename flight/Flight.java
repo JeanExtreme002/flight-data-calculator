c253095dfef81975b2fe3cb60e0cb8965dbf06cc
JeanExtreme002 <jeangamerextreme@gmail.com>
2021-06-01 14:30:36 -0300

    ETA changed to ETE

--- flight/Flight.java
+++ flight/Flight.java
@@ -4,51 +4,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Flight {
-	private List<Waypoint> waypoints = new ArrayList<>();
-	private float averageFuelConsumption;
+    private List<Waypoint> waypoints = new ArrayList<>();
+    private float averageFuelConsumption;
 
-	public Flight(float averageFuelConsumption) {
-		this.averageFuelConsumption = averageFuelConsumption;
-	}
+    public Flight(float averageFuelConsumption) {
+        this.averageFuelConsumption = averageFuelConsumption;
+    }
 
-	private FlightLeg getFlightLeg(int index) {
-		return new FlightLeg(this.waypoints.get(index), this.waypoints.get(index + 1));
-	}
+    private FlightLeg getFlightLeg(int index) {
+        return new FlightLeg(this.waypoints.get(index), this.waypoints.get(index + 1));
+    }
 
-	public void addWaypoint(Waypoint waypoint) {
-		this.waypoints.add(waypoint);
-	}
+    public void addWaypoint(Waypoint waypoint) {
+        this.waypoints.add(waypoint);
+    }
 
-	public List<FlightLeg> getLegs() {
-		List<FlightLeg> legs = new ArrayList<>();
+    public List<FlightLeg> getLegs() {
+        List<FlightLeg> legs = new ArrayList<>();
 
-		for (int index = 0; index < this.waypoints.size() - 1; index++) {
-			legs.add(this.getFlightLeg(index));
-		}
-		return legs;
-	}
+        for (int index = 0; index < this.waypoints.size() - 1; index++) {
+            legs.add(this.getFlightLeg(index));
+        }
+        return legs;
+    }
 
-	public float getEstimatedFuel(FlightLeg flightLeg) {
-		// Converts the consumption per hour to consumption per minute.
-		float averageFuelConsumption = this.averageFuelConsumption / 60;
-		return averageFuelConsumption * flightLeg.getEstimatedTime();
-	}
+    public float getEstimatedFuel(FlightLeg flightLeg) {
+        // Converts the consumption per hour to consumption per minute.
+        float averageFuelConsumption = this.averageFuelConsumption / 60;
+        return averageFuelConsumption * flightLeg.getEstimatedTime();
+    }
 
-	public float getEstimatedFuel() {
-		float total = 0;
+    public float getEstimatedFuel() {
+        float total = 0;
 
-		for (FlightLeg flightLeg : this.getLegs()) {
-			total += this.getEstimatedFuel(flightLeg);
-		}
-		return total;
-	}
+        for (FlightLeg flightLeg : this.getLegs()) {
+            total += this.getEstimatedFuel(flightLeg);
+        }
+        return total;
+    }
 
-	public float getEstimatedTime() {
-		float total = 0;
+    public float getEstimatedTime() {
+        float total = 0;
 
-		for (FlightLeg flightLeg : this.getLegs()) {
-			total += flightLeg.getEstimatedTime();
-		}
-		return total;
-	}
+        for (FlightLeg flightLeg : this.getLegs()) {
+            total += flightLeg.getEstimatedTime();
+        }
+        return total;
+    }
 }
--- flight/FlightLeg.java
+++ flight/FlightLeg.java
@@ -1,69 +1,69 @@
 package flight;
 
 public final class FlightLeg {
-	private Waypoint origin;
-	private Waypoint destination;
+    private Waypoint origin;
+    private Waypoint destination;
 
-	public FlightLeg(Waypoint origin, Waypoint destination) {
-		this.origin = origin;
-		this.destination = destination;
-	}
+    public FlightLeg(Waypoint origin, Waypoint destination) {
+        this.origin = origin;
+        this.destination = destination;
+    }
 
-	private double getEstimatedAcceleration() {
-		float distance = this.getDistanceInMeters();
-		float initialGroundSpeed = this.origin.getGroundSpeedInMetersPerSecond();
-		float finalGroundSpeed = this.destination.getGroundSpeedInMetersPerSecond();
+    private double getEstimatedAcceleration() {
+        float distance = this.getDistanceInMeters();
+        float initialGroundSpeed = this.origin.getGroundSpeedInMetersPerSecond();
+        float finalGroundSpeed = this.destination.getGroundSpeedInMetersPerSecond();
 
-		/* Torricelli's equation:
-		   Final Velocity ^ 2 = Initial Velocity ^ 2 + 2 * Acceleration * Distance
-		   Acceleration = (Final Velocity ^ 2 - Initial Velocity ^ 2) / (2 * Distance)
-		*/
-		return (Math.pow(finalGroundSpeed, 2) - Math.pow(initialGroundSpeed, 2)) / (2 * distance);
-	}
+        /* Torricelli's equation:
+           Final Velocity ^ 2 = Initial Velocity ^ 2 + 2 * Acceleration * Distance
+           Acceleration = (Final Velocity ^ 2 - Initial Velocity ^ 2) / (2 * Distance)
+        */
+        return (Math.pow(finalGroundSpeed, 2) - Math.pow(initialGroundSpeed, 2)) / (2 * distance);
+    }
 
-	private float getDistanceInMeters() {
-	 	float distance = this.destination.getDistanceInMeters() - this.origin.getDistanceInMeters();
-		return distance < 0 ? distance * -1 : distance; // Returns a positive number.
-	}
+    private float getDistanceInMeters() {
+         float distance = this.destination.getDistanceInMeters() - this.origin.getDistanceInMeters();
+        return distance < 0 ? distance * -1 : distance; // Returns a positive number.
+    }
 
-	public String toString() {
-		String route = this.origin.getId() + " -> " + this.destination.getId();
-		return "FlightLeg(" + route + " | " + this.getDistance() + "NM" + ")";
-	}
+    public String toString() {
+        String route = this.origin.getId() + " -> " + this.destination.getId();
+        return "FlightLeg(" + route + " | " + this.getDistance() + "NM" + ")";
+    }
 
-	public float getDistance() {
-	 	float distance = this.destination.getDistance() - this.origin.getDistance();
-		return distance < 0 ? distance * -1 : distance; // Returns a positive number.
-	}
+    public float getDistance() {
+         float distance = this.destination.getDistance() - this.origin.getDistance();
+        return distance < 0 ? distance * -1 : distance; // Returns a positive number.
+    }
 
-	/**
-	* Returns the estimated time in minutes.
-	*/
-	public float getEstimatedTime() {
-		float initialGroundSpeed = this.origin.getGroundSpeedInMetersPerSecond();
-		float finalGroundSpeed = this.destination.getGroundSpeedInMetersPerSecond();
-		double acceleration = this.getEstimatedAcceleration();
+    /**
+    * Returns the estimated time in minutes.
+    */
+    public float getEstimatedTime() {
+        float initialGroundSpeed = this.origin.getGroundSpeedInMetersPerSecond();
+        float finalGroundSpeed = this.destination.getGroundSpeedInMetersPerSecond();
+        double acceleration = this.getEstimatedAcceleration();
 
-		/* Final Velocity Formula:
-		   Final Velocity = Acceleration * Time + Initial Velocity
-		   Time = (Final Velocity - Initial Velocity) / Acceleration
-		*/
-		return (float) ((finalGroundSpeed - initialGroundSpeed) / acceleration / 60);
-	}
+        /* Final Velocity Formula:
+           Final Velocity = Acceleration * Time + Initial Velocity
+           Time = (Final Velocity - Initial Velocity) / Acceleration
+        */
+        return (float) ((finalGroundSpeed - initialGroundSpeed) / acceleration / 60);
+    }
 
-	/**
-	* Returns the estimated vertical speed in feet per minute.
-	*/
-	public float getEstimatedVerticalSpeed() {
-		float altitude = this.origin.getAltitude() - this.destination.getAltitude();
-		return (altitude / this.getEstimatedTime()) * -1;
-	}
+    /**
+    * Returns the estimated vertical speed in feet per minute.
+    */
+    public float getEstimatedVerticalSpeed() {
+        float altitude = this.origin.getAltitude() - this.destination.getAltitude();
+        return (altitude / this.getEstimatedTime()) * -1;
+    }
 
-	public Waypoint getOrigin() {
-		return this.origin;
-	}
+    public Waypoint getOrigin() {
+        return this.origin;
+    }
 
-	public Waypoint getDestination() {
-		return this.destination;
-	}
+    public Waypoint getDestination() {
+        return this.destination;
+    }
 }
